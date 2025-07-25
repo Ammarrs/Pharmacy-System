50db7dcb3048532906cb4061e345024e10fc6ca1
Ibrahim-Ashraf95 <abrahim1234.ashraf1234@gmail.com>
2025-07-25 22:17:33 +0300

    Add classes

---
+++ Product.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pharmacy_system;
+
+/**
+ *
+ * @author elkhedewy-group
+ */
+public class Product {
+    protected int productId;
+    protected String name;
+    protected double price;
+    protected int quantity;
+
+    public Product(int productId, String name, double price, int quantity) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    public boolean isAvailable(int qty){
+        return quantity >= qty;
+    }
+    
+    public void reduceQuantity(int qty){
+        if(isAvailable(qty))
+            quantity -= qty;
+    }
+}
+
+public class Cosmetic extends Product {
+    
+}
