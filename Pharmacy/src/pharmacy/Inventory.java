46933c6166d10834759977a0c923dc4b27c45fd1
Ammar Meshaal <157367715+Ammarrs@users.noreply.github.com>
2025-08-15 08:06:20 +0300

    applying file handling on inventory

--- Pharmacy/src/pharmacy/Inventory.java
+++ Pharmacy/src/pharmacy/Inventory.java
@@ -4,6 +4,13 @@
  */
 package pharmacy;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 import pharmacy.Product;
@@ -13,16 +20,23 @@ import pharmacy.Product;
  */
 public class Inventory {
     private List<Product> products = new ArrayList<>();
+    private static final String FILE_NAME = "inventory.txt";
+    
+    public Inventory() {
+        loadFromFile();
+    }
     
     void addProduct (Product prod) {
         // Make changes
         for (Product p: products) {
             if (p.getProductId() == prod.getProductId()) {
                 p.setQuantity(p.getQuantity() + prod.getQuantity());
+                saveToFile();
                 return;
             }
         }
         products.add(prod);
+        saveToFile();
     }
     
     void updateQuantity (Product prod, int qtty) {
@@ -31,6 +45,7 @@ public class Inventory {
             if (p.getProductId() == prod.getProductId()) {
                 p.setQuantity(p.getQuantity() + qtty);
                 System.out.println("[INFO] Quantity of " + p.getName() + " updated to " + p.getQuantity());
+                saveToFile();
                 return;
             }
         }
@@ -68,5 +83,42 @@ public class Inventory {
                     );
         }
         System.out.println("=====================================");
+    }
+    
+    // for files
+    
+    private void saveToFile() {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME))) {
+            for (Product p: products) {
+                writer.write(p.getProductId() + "," + p.getName() + "," + p.getPrice() + "," + p.getQuantity());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("[ERR], Failedto save Inventory: " + e.getMessage());
+        }
+    }
+    
+    private void loadFromFile() {
+        File file = new File(FILE_NAME);
+        if(!file.exists()) {
+            return;
+        }
+        
+        try(BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+            line = reader.readLine();
+            while(line != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 4) {
+                    int id = Integer.parseInt(parts[0]);
+                    String name = parts[1];
+                    double price = Double.parseDouble(parts[2]);
+                    int quantity = Integer.parseInt(parts[3]);
+                    products.add(new Product(id, name, price, quantity));
+                }
+            }
+        } catch (IOException e){
+            System.out.println("[ERR], Failed to load Inventory " + e.getMessage());
+        }
     }
 }
